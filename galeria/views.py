dfcb8594342fb7047714aaf4ed4b979ff9c61cf8
Matheus <limarios18@outlook.com>
2024-07-03 18:50:10 -0300

    Implementando acesso ao banco e usando dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
 
-dados = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelecope.org / NASA / James Webb"},
-    2: { "nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}
-}
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-            "legenda": "webbtelecope.org / NASA / James Webb"},
-        2: { "nome": "Galáxia NGC 1079",
-            "legenda": "nasa.org / NASA / Hubble"}
-    }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html' )
