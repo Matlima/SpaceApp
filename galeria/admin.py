2f41cb8f00bd86974c861940ed4ea85e3024144a
Matheus <limarios18@outlook.com>
2024-07-04 23:53:05 -0300

    Implementando Admin na aplicação

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+# Personalizando admin:
+class ListandoFotografias(admin.ModelAdmin):
+    # Exibindo campos inves do nome do objeto
+    list_display = ("id", "nome", "legenda")
+    # Adicionando link ao clicar na lista da tabela
+    list_display_links = ("id", "nome")
+    # Adicionando campo de busca no admin:
+    search_fields = ("nome",)
+
+# Envio de dados para mostrar no admin:
+admin.site.register(Fotografia, ListandoFotografias)
+
+
